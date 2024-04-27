97171a42670e242c29aa750d0afb67c53c340d6f
Ashin Jimmy <118276636+ashinjimmy@users.noreply.github.com>
2024-04-27 19:23:14 +0100

    Update DepartmentRepositoryTest.java

--- src/test/java/com/tus/department/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/tus/department/repository/DepartmentRepositoryTest.java
@@ -1,10 +1,6 @@
 package com.tus.department.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
@@ -12,10 +8,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import com.tus.department.entity.DepartmentEntity;
 
 @DataJpaTest
-public class DepartmentRepositoryTest {
+ class DepartmentRepositoryTest {
 
-	@Autowired
-	private DepartmentRepository departmentRepository;
 
 	@Autowired
 	private TestEntityManager testEntityManager;
@@ -29,12 +23,5 @@ public class DepartmentRepositoryTest {
 		testEntityManager.persist(department);
 	}
 	
-	@Test
-	@Disabled
-	public void whenFindById_thenReturnDepartment() {
-		DepartmentEntity department =  departmentRepository.findById(1L).get();
-		
-		assertEquals(department.getDepartmentName(), "Mech");
-	}
 	
 }
