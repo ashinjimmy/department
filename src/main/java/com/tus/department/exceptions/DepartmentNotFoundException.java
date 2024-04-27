fa5ea83dda0e5bcf9ec67dba735ea5f27978cfb8
Ashin Jimmy <118276636+ashinjimmy@users.noreply.github.com>
2024-04-27 18:05:52 +0100

    Update DepartmentNotFoundException.java

--- src/main/java/com/tus/department/exceptions/DepartmentNotFoundException.java
+++ src/main/java/com/tus/department/exceptions/DepartmentNotFoundException.java
@@ -2,6 +2,8 @@ package com.tus.department.exceptions;
 
 public class DepartmentNotFoundException extends Exception {
 
+	private static final long serialVersionUID = 1L;
+
 	public DepartmentNotFoundException() {
 		super();
 	}
