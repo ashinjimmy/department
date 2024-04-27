da43b8116197dd503fac75f0d5e473cc9182672a
Ashin Jimmy <118276636+ashinjimmy@users.noreply.github.com>
2024-04-27 19:24:52 +0100

    Update DataScheduler.java

--- src/main/java/com/tus/department/scheduler/DataScheduler.java
+++ src/main/java/com/tus/department/scheduler/DataScheduler.java
@@ -1,32 +1,10 @@
 package com.tus.department.scheduler;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
-
-import com.google.gson.Gson;
-import com.tus.department.entity.DepartmentEntity;
-import com.tus.department.service.DepartmentService;
 
 @Component
 //@EnableScheduling
 public class DataScheduler {
 
-	private DepartmentService departmentService;
-
-	public DataScheduler(DepartmentService departmentService) {
-		this.departmentService = departmentService;
-	}
-
-	//@Scheduled(fixedRate = 3000)
-	public String fetchAndProccess() {
-		System.out.println("Schedule Active!!!");
-
-		List<DepartmentEntity> deptData = departmentService.fetchAllDepartments();
-		String jsonObj = new Gson().toJson(deptData);
-		System.out.println(jsonObj);
-		
-		return "Schedule Active!!!";
-	}
 
 }
