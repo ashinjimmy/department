fca9af43a716430178aff8481e552299251bb8ee
Ashin Jimmy <118276636+ashinjimmy@users.noreply.github.com>
2024-04-27 19:32:34 +0100

    Update DepartmentRepositoryTest.java

--- src/test/java/com/tus/department/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/tus/department/repository/DepartmentRepositoryTest.java
@@ -1,27 +1,5 @@
 package com.tus.department.repository;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+class DepartmentRepositoryTest {
 
-import com.tus.department.entity.DepartmentEntity;
-
-@DataJpaTest
- class DepartmentRepositoryTest {
-
-
-	@Autowired
-	private TestEntityManager testEntityManager;
-
-	@BeforeEach
-	void setUp() {
-
-		DepartmentEntity department = DepartmentEntity.builder().departmentId(1L).departmentName("Mech").departmentAddress("Delhi")
-				.departmentCode("MECH").build();
-
-		testEntityManager.persist(department);
-	}
-	
-	
 }
